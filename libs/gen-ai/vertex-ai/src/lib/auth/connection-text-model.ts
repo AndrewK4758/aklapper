67d827aee9f33bc94da310bebdb4dfd96c5b8654
Andrew <ak475826@gmail.com>
2025-01-24 21:32:42 -0500

    try 8 - fixed package.json issue in crud-api docker build

--- libs/gen-ai/vertex-ai/src/lib/auth/connection-text-model.ts
+++ libs/gen-ai/vertex-ai/src/lib/auth/connection-text-model.ts
@@ -5,8 +5,8 @@ import { cwd } from 'process';
 configDotenv({ path: `${cwd()}/env/.env` });
 
 // MY STUFF
-export const PROJECT = process.env['GCP_PROJECT'];
-export const LOCATION = process.env['GCP_LOCATION'];
+export const PROJECT = 'games-424800';
+export const LOCATION = 'us-central1';
 
 export const MODEL = 'gemini-1.5-pro-001';
 
