a2420ac5d2f359ed77b113f52548ee409f533f27
Andrew Klapper <andrew@andrew-k.us>
2025-07-03 22:07:07 -0400

    try 3

--- apps/react/portfolio/portfolio/vite_modules.ts
+++ apps/react/portfolio/portfolio/vite_modules.ts
@@ -9,12 +9,13 @@ const MODULES: ViteModules = {
   '@aklapper/chinook-client': resolve(workspaceRoot, 'packages/prisma/chinook/src/index.ts'),
 
   '@aklapper/games-components': resolve(workspaceRoot, 'packages/games-components/src/index.ts'),
+  '@aklapper/games-components': resolve(workspaceRoot, 'packages/games-components/src'),
 
-  '@aklapper/media-recorder': resolve(workspaceRoot, 'packages/media-recorder/src/index.ts'),
+  '@aklapper/media-recorder': resolve(workspaceRoot, 'packages/media-recorder/src'),
 
-  '@aklapper/prompt-builder': resolve(workspaceRoot, 'packages/gen-ai/prompt-builder/src/index.ts'),
+  '@aklapper/prompt-builder': resolve(workspaceRoot, 'packages/gen-ai/prompt-builder/src'),
 
-  '@aklapper/react-shared': resolve(workspaceRoot, 'packages/react-shared/src/index.ts'),
+  '@aklapper/react-shared': resolve(workspaceRoot, 'packages/react-shared/src'),
 
   '@aklapper/types': resolve(workspaceRoot, 'packages/types/src/index.ts'),
 
