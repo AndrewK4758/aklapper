5089dc413c0cf9e10ce88e13d94c31bff529d3ba
Andrew Klapper <andrew@andrew-k.us>
2025-07-03 22:07:07 -0400

    try 3

--- apps/react/portfolio/portfolio/src/components/landing/box_animation.tsx
+++ apps/react/portfolio/portfolio/src/components/landing/box_animation.tsx
@@ -1,32 +1,6 @@
-import { keyframes } from '@mui/material-pigment-css';
 import Box from '@mui/material-pigment-css/Box';
+import { shake } from '../../styles/base/animations';
 import LandingBox from '../icons/landing_box_icon';
-
-const shake = keyframes`
-  1.25%,
-  11.25% {
-    transform: rotateZ(-0.75deg);
-  }
-  2.5%,
-  10% {
-    transform: rotateZ(1.5deg);
-  }
-  3.75%,
-  6.75%,
-  8.75% {
-    transform: rotateZ(-3deg);
-  }
-  5%,
-  7.5% {
-    transform: rotateZ(3deg);
-  }
-  11.5% {
-    transform: rotateZ(0deg);
-  }
-  100% {
-    transition: rotateZ(0deg);
-  }
-`;
 
 const landingBoxBaseStyle = {
   '@media (max-width: 1200px)': {
--- apps/react/portfolio/portfolio/vite.config.ts
+++ apps/react/portfolio/portfolio/vite.config.ts
@@ -5,8 +5,7 @@ import { resolve } from 'node:path';
 import { cwd } from 'node:process';
 import { defineConfig, type UserConfig } from 'vite';
 import Theme from './src/styles/themes/theme';
-import MODULES from './vite_modules';
-process.env.NODE_ENV = 'development';
+// import MODULES from './vite_modules';
 
 //Server
 const HOST = 'localhost';
@@ -52,7 +51,7 @@ const config: UserConfig = defineConfig({
   // },
 
   resolve: {
-    alias: MODULES,
+    // alias: MODULES,
     conditions: ['mui-modern', 'module', 'browser', 'development|production'],
   },
 
