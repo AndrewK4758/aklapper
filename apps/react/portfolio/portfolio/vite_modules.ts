64d50d37cedb643308e538c4fb5381f4a3c16d42
Andrew Klapper <andrew@andrew-k.us>
2025-07-03 22:29:50 -0400

    try 6

--- apps/react/portfolio/portfolio/vite_modules.ts
+++ apps/react/portfolio/portfolio/vite_modules.ts
@@ -10,11 +10,11 @@ const MODULES: ViteModules = {
 
   '@aklapper/games-components': resolve(workspaceRoot, 'packages/games-components/src/index.ts'),
 
-  '@aklapper/media-recorder': resolve(workspaceRoot, 'packages/media-recorder'),
+  '@aklapper/media-recorder': resolve(workspaceRoot, 'packages/media-recorder/src/index.ts'),
 
-  '@aklapper/prompt-builder': resolve(workspaceRoot, 'packages/gen-ai/prompt-builder'),
+  '@aklapper/prompt-builder': resolve(workspaceRoot, 'packages/gen-ai/prompt-builder/src/index.ts'),
 
-  '@aklapper/react-shared': resolve(workspaceRoot, 'packages/react-shared'),
+  '@aklapper/react-shared': resolve(workspaceRoot, 'packages/react-shared/src/index.ts'),
 
   '@aklapper/types': resolve(workspaceRoot, 'packages/types/src/index.ts'),
 
