66f3471fcdba8674a240c123017b0faee04e14e4
Andrew Klapper <andrew@andrew-k.us>
2025-07-03 22:23:47 -0400

    try 5

--- apps/react/portfolio/portfolio/vite_modules.ts
+++ apps/react/portfolio/portfolio/vite_modules.ts
@@ -6,6 +6,8 @@ type ViteModules = {
 };
 
 const MODULES: ViteModules = {
+  '@aklapper/chinook-client': resolve(workspaceRoot, 'packages/prisma/chinook'),
+
   '@aklapper/games-components': resolve(workspaceRoot, 'packages/games-components'),
 
   '@aklapper/media-recorder': resolve(workspaceRoot, 'packages/media-recorder'),
@@ -15,6 +17,8 @@ const MODULES: ViteModules = {
   '@aklapper/react-shared': resolve(workspaceRoot, 'packages/react-shared'),
 
   '@aklapper/types': resolve(workspaceRoot, 'packages/types'),
+
+  '@aklapper/vertex-ai': resolve(workspaceRoot, 'packages/gen-ai/vertex-ai'),
 };
 
 export default MODULES;
