f726f2948da3bd5d2fd1c2718ae7f492221377e2
Andrew Klapper <andrew@andrew-k.us>
2025-07-05 13:46:15 -0400

    fix: fixed small portfolio styles and added the submit function for email me

--- apps/react/portfolio/portfolio/src/services/loaders/crud-loaders/load-artists.ts
+++ apps/react/portfolio/portfolio/src/services/loaders/crud-loaders/load-artists.ts
@@ -5,19 +5,18 @@ import type { LoaderFunction, LoaderFunctionArgs } from 'react-router';
 
 const baseURL = import.meta.env.VITE_CRUD_API_URL;
 
-const loadArtists = async (queryOptions: QueryOptions, signal: AbortSignal): Promise<artist[]> => {
+const loadArtistsCount: LoaderFunction = async ({
+  request,
+}: LoaderFunctionArgs): Promise<DataGridLoaderWithCount<artist[]> | void> => {
   try {
-    const { take, skip, cursor } = queryOptions;
-    const resp = await axios.get(`${baseURL}/artists?take=${take}&skip=${skip}&cursor=${cursor}`, {
-      headers: { 'Content-Type': 'text/plain' },
-      signal,
-    });
+    const { search } = new URL(request.url);
 
-    const { data } = resp.data;
-    return data as artist[];
+    const resp = await axios.get(`${baseURL}/artists${search}`);
+    const { count, data } = resp.data;
+
+    return { count, data };
   } catch (error) {
     console.error(error);
-    throw error;
   }
 };
 
