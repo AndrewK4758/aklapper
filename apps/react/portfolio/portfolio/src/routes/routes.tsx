53217c1240ff438419d8f21561e1d437654c06cc
Andrew Klapper <andrew@andrew-k.us>
2025-07-06 00:11:28 -0400

    refactor: crud artist data grid to only use react rotuer loaders and fetcher

--- apps/react/portfolio/portfolio/src/routes/routes.tsx
+++ apps/react/portfolio/portfolio/src/routes/routes.tsx
@@ -17,7 +17,6 @@ import Crud from '../pages/crud/crud';
 import Games from '../pages/games/games';
 import GenAiHome from '../pages/gen-ai/gen-ai';
 
-import handleArtistAlbumsActions from '../services/actions/crud-actions/handle_album_actions.js';
 import handleArtistActions from '../services/actions/crud-actions/handle_artist_actions.js';
 import handleTrackActions from '../services/actions/crud-actions/handle_track_actions';
 import generateImageAction from '../services/actions/generate-image-action';
@@ -28,7 +27,6 @@ import handleTrackActions from '../services/actions/crud-actions/handle_track_ac
 import generateImageAction from '../services/actions/generate-image-action';
 
 import registerPlayersAndStartGame from '../services/loaders/register-players-and-start-game';
-
 const ActiveGameSession = lazy(() => import('../components/games/active_game_session'));
 
 const AddEntry = lazy(() => import('../components/crud/add-entry/add-entry'));
@@ -76,22 +74,17 @@ export const routes: RouteObject[] = [
           {
             path: 'artists',
             Component: Artist,
-            loader: loadArtistsAndCount,
-            id: 'artists',
+            loader: loadArtistsCount,
             action: handleArtistActions,
             children: [
               {
                 path: ':artistID/albums',
                 Component: AlbumsOnArtist,
-                id: 'artist_albums',
-                action: handleArtistAlbumsActions,
-                loader: loadAlbumsForArtist,
                 children: [
                   {
                     path: ':albumID/tracks',
                     Component: Tracks,
-                    action: handleTrackActions,
-                    loader: loadAlbumTracks,
+                    loader: loadTracksCount,
                   },
                 ],
               },
@@ -100,12 +93,12 @@ export const routes: RouteObject[] = [
           {
             path: 'albums',
             Component: Album,
-            loader: loadAlbums,
+            loader: loadAlbumsCount,
             children: [
               {
                 path: ':albumID/tracks',
                 Component: Tracks,
-                loader: loadAlbumTracks,
+                loader: loadTracksCount,
               },
             ],
           },
