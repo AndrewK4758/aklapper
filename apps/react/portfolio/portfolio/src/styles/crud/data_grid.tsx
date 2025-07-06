6a637c6ffb3e6d2caab346254970e5b382b46bef
Andrew Klapper <andrew@andrew-k.us>
2025-07-06 00:11:28 -0400

    refactor: crud artist data grid to only use react rotuer loaders and fetcher

--- apps/react/portfolio/portfolio/src/styles/crud/data_grid.tsx
+++ apps/react/portfolio/portfolio/src/styles/crud/data_grid.tsx
@@ -2,6 +2,16 @@ import type { SxProp } from '@pigment-css/react';
 import { BACKGROUND_PAPER, BASE_BORDER_RADIUS } from '../base/base_styles';
 import Theme from '../themes/theme';
 
+export const allDataGridsWrapperSxProps: SxProp = {
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+  padding: 0,
+  gap: Theme.spacing(4),
+  border: 2,
+  width: '100%',
+};
+
 export const artistsSxProps: SxProp = {
   flex: 1,
   gap: Theme.spacing(4),
