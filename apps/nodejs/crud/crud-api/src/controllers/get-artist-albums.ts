bcda3c2c89d60778350be60daa43fab4c52bb6f4
Andrew Klapper <andrew@andrew-k.us>
2025-07-06 02:07:12 -0400

    update: updated album data grid to react router v7 data patterns

--- apps/nodejs/crud/crud-api/src/controllers/get-artist-albums.ts
+++ apps/nodejs/crud/crud-api/src/controllers/get-artist-albums.ts
@@ -1,7 +1,7 @@
 import { Prisma, type album } from '@aklapper/chinook-client';
 import type { CRUD_ApiResponse } from '@aklapper/types';
 import type { DefaultArgs } from '@prisma/client/runtime/library';
-import type { NextFunction, Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import getArtistAlbums from '../services/prisma/album/get-artist-albums.js';
 
 /**
@@ -15,27 +15,28 @@ import getArtistAlbums from '../services/prisma/album/get-artist-albums.js';
  * @returns No explicit return value. It either sends a JSON response with the artist's albums or calls the `next()` middleware function.
  */
 
-const getArtistsAlbums = async (req: Request, resp: Response, next: NextFunction): Promise<void> => {
-  if (!req.query.take) next();
-  else {
-    try {
-      const artistID = req.params.id;
+const getArtistsAlbums = async (req: Request, resp: Response): Promise<void> => {
+  try {
+    const artistID = req.params.id;
+    const { cursor, skip, take } = req.query;
 
-      const query = {
-        where: { artist_id: parseInt(artistID, 10) },
-      } as Prisma.albumFindManyArgs<DefaultArgs>;
+    console.log('ARTIST ID: ', artistID);
+    console.log(cursor, skip, take);
 
-      const data = await getArtistAlbums(query);
+    const query = {
+      where: { artist_id: parseInt(artistID, 10) },
+    } as Prisma.albumFindManyArgs<DefaultArgs>;
 
-      const respData: CRUD_ApiResponse<album[]> = {
-        message: 'Arist Albums found',
-        data: data,
-      };
-      resp.status(200).json(respData);
-    } catch (error) {
-      console.error(error);
-      resp.status(500).json(error);
-    }
+    const { count, data } = await getArtistAlbums(query);
+
+    const respData: CRUD_ApiResponse<{ count: number; data: album[] }> = {
+      message: 'Arist Albums found',
+      value: { count, data },
+    };
+    resp.status(200).json(respData);
+  } catch (error) {
+    console.error(error);
+    resp.status(500).json(error);
   }
 };
 
