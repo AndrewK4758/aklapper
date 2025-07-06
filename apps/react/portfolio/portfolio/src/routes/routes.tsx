5c4927bcf4cbd55fe726f64e551edfaa02d2b2d6
Andrew Klapper <andrew@andrew-k.us>
2025-07-06 02:07:12 -0400

    update: updated album data grid to react router v7 data patterns

--- apps/react/portfolio/portfolio/src/routes/routes.tsx
+++ apps/react/portfolio/portfolio/src/routes/routes.tsx
@@ -18,12 +18,14 @@ import Layout from '../components/layout/layout';
 import Crud from '../pages/crud/crud';
 import Games from '../pages/games/games';
 import GenAiHome from '../pages/gen-ai/gen-ai';
+import generateImageAction from '../services/actions/generate-image-action';
 
 import handleArtistActions from '../services/actions/crud-actions/handle_artist_actions.js';
 import handleTrackActions from '../services/actions/crud-actions/handle_track_actions';
 import generateImageAction from '../services/actions/generate-image-action';
 
 import registerPlayersAndStartGame from '../services/loaders/register-players-and-start-game';
+
 const ActiveGameSession = lazy(() => import('../components/games/active_game_session'));
 
 const AddEntry = lazy(() => import('../components/crud/add-entry/add-entry'));
@@ -71,12 +73,16 @@ export const routes: RouteObject[] = [
           {
             path: 'artists',
             Component: Artist,
-            loader: loadArtistsCount,
+            loader: loadArtistsAndCount,
+            id: 'artists',
             action: handleArtistActions,
             children: [
               {
                 path: ':artistID/albums',
                 Component: AlbumsOnArtist,
+                id: 'artist_albums',
+                action: handleArtistAlbumsActions,
+                loader: loadAlbumsForArtistAndCount,
                 children: [
                   {
                     path: ':albumID/tracks',
@@ -90,7 +96,7 @@ export const routes: RouteObject[] = [
           {
             path: 'albums',
             Component: Album,
-            loader: loadAlbumsCount,
+            // loader: loadAlbumsCount,
             children: [
               {
                 path: ':albumID/tracks',
