047510725234f288124c0874b96979790054b708
Andrew Klapper <andrew@andrew-k.us>
2025-07-07 19:48:01 -0400

    refactor: changed all crud data grid to react router loaders

--- apps/react/portfolio/portfolio/src/components/crud/artists/artist-base.tsx
+++ apps/react/portfolio/portfolio/src/components/crud/artists/artist-base.tsx
@@ -1,5 +1,8 @@
+// import type { artist } from '@aklapper/chinook-client';
 import type { artist } from '@aklapper/chinook-client';
 import { Waiting } from '@aklapper/react-shared';
+import type { DataGridLoaderWithCount } from '@aklapper/types';
+import { css } from '@mui/material-pigment-css';
 import Box from '@mui/material-pigment-css/Box';
 import Container from '@mui/material-pigment-css/Container';
 import { type ReactElement, Suspense } from 'react';
@@ -7,7 +10,6 @@ import { Outlet, useFetcher, useLoaderData } from 'react-router';
 import waiting from '../../../assets/images/swirly-dots-to-chrome.webp';
 import { artistsSxProps } from '../../../styles/crud/data_grid';
 import Theme from '../../../styles/themes/theme';
-import { ArtistLoader } from '../../../types/types';
 import CenteredFlexDiv from '../../styled/centered_flexbox';
 import DataGridHeader from '../data_grid_header';
 import AddArtist from './add-artist';
@@ -21,9 +23,8 @@ import ArtistDataGrid from './data_grid';
  */
 
 const Artist = (): ReactElement => {
-  const COUNT = useLoaderData() as number;
-  const [rows, setRows] = useState<artist[] | null>(null);
-  const [rowCountState, setRowCountState] = useState(COUNT);
+  const { count, data } = useLoaderData<DataGridLoaderWithCount<artist[]>>();
+  const fetcher = useFetcher();
 
   return (
     <CenteredFlexDiv
@@ -48,8 +49,8 @@ const Artist = (): ReactElement => {
           <ArtistDataGrid rows={data} COUNT={count} fetcher={fetcher} />
         </Box>
       </Box>
-      <Suspense fallback={<Waiting src={waiting} />}>
-        <Box sx={{ flex: '0 1 50%' }}>
+      <Box sx={{ flex: '0 1 100%' }}>
+        <Suspense fallback={<Waiting src={waiting} />}>
           <Outlet />
         </Suspense>
       </Box>
