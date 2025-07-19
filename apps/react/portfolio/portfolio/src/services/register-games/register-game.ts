b5196096ff9984b453be6f7e10958e4564aa61cb
Andrew Klapper <ak475826@gmail.com>
2025-07-18 23:26:22 -0400

    portfolio_v2 - pull for prod (#28)
    * changed back to vite from rolldown vite
    
    * DO NOT IMPORT GRID FROM PIGMENT CSS AS IT WILL BREAK VITEST TESTS
    
    * updated style for tech list and home text
    
    * trying echh >> github_env for vite env variables
    
    * trying echo >> github_env for vite env variables
    
    * trying echo >> github_env for vite env variables
    
    * trying echo >> github_env for vite env variables
    
    * changed vite env to global env config
    
    * changed vite env to global env config
    
    * changed vite env to global env config
    
    ---------
    
    Co-authored-by: Andrew Klapper <andrew@andrew-k.us>

--- apps/react/portfolio/portfolio/src/services/register-games/register-game.ts
+++ apps/react/portfolio/portfolio/src/services/register-games/register-game.ts
@@ -3,11 +3,16 @@ import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_GAMES_API_URL;
 
+console.log(baseUrl);
+
 const registerGame = async (gameName: string): Promise<GamePlayerValidation | void> => {
   try {
     const resp = await axios.post(`${baseUrl}/games/${gameName}`);
 
     const gameData: GamePlayerValidation = JSON.parse(resp.headers['current-game']);
+
+    console.log(gameData);
+
     return gameData;
   } catch (error) {
     console.error(error);
